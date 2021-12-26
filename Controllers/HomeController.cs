eeba612adc24f893b5ab5babdce1d6db4f1fc5aa
Александр Поликарпов <C:\Users\kgb\AppData\Roaming\The Bat!>
2021-12-26 22:16:50 +0200

    initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TestWebApp.Models;
+
+namespace TestWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IConfiguration configuration;
+
+        public HomeController(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.result1 = configuration["MyKey_appsettings"];
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
